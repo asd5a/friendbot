c10fa7b4828e46f07a8bb52c95fb56172aa69036
MorizSchildorfer <moriz.schildorfer@gmail.com>
2023-04-01 20:07:44 +0200

    Update admin.py

--- cogs/admin.py
+++ cogs/admin.py
@@ -77,14 +77,12 @@ class Admin(commands.Cog, name="Admin"):
         message = await ch.fetch_message(msg)
         await message.add_reaction(emote)
         await ctx.message.delete()
-    
-    @react.command()
-    async def hitme(self, ctx):
-        await ctx.message.add_reaction('633820509053648908')
         
     @react.command()
     async def boopme(self, ctx):
         await ctx.message.add_reaction('<a:owoo:633820509053648908>')
+        await ctx.message.add_reaction('<a:owow:633820509540188176>')
+        await ctx.message.add_reaction('<a:owooo:634494313040183316>')
     
     #Allows the sending of messages
     @commands.command()
